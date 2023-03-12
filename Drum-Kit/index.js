c62f7ad40374ec6ad1876081a81f19fae40357ac
Prashant Mishra <65122711+invader1player@users.noreply.github.com>
2023-03-12 11:41:34 +0530

    Create

---
+++ Drum-Kit/index.js
@@ -0,0 +1,59 @@
+var numofDrumbtn=document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numofDrumbtn; i++){
+
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        btnn=this.innerHTML;
+        makeSound(btnn);
+        btnAnimation(btnn);
+    });
+}
+
+addEventListener("keypress",function(event)
+{
+    makeSound(event.key);
+    btnAnimation(event.key);
+});
+
+function makeSound(key)
+{
+    switch (key){
+        case "w":
+            var audio1= new Audio("sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "s":
+            var audio2= new Audio("sounds/tom-2.mp3");
+            audio2.play();
+            break;
+        case "a":
+            var audio3= new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+        case "d":
+            var audio4= new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+        case "j":
+            var audio5= new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            var audio6= new Audio("sounds/crash.mp3");
+            audio6.play();
+            break;
+        case "l":
+            var audio7= new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break;
+        default: console.log();
+        }
+}
+
+function btnAnimation(currentkey){
+    var activebtn=document.querySelector('.'+currentkey);
+    activebtn.classList.add("pressed");
+    setTimeout(function(){
+        activebtn.classList.remove("pressed");
+    },100);
+}
